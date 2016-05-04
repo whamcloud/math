edf37b65833e4f0206777f9b8ac36583b792581b
Joe Grund <joe.grund@intel.com>
2016-05-04 14:37:17 -0400

    HYD-5938 Math: bump deps to latest
    Bump math and it's deps to latest available versions.
    
      - Bump deps.
      - Update module to be closer to node-babel-boilerplate.
      - Remove shrinkwrap as we should be able to mitigate with cache.
      - Fixup issues.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- source/math.js
+++ source/math.js
@@ -23,18 +23,18 @@
 
 import {curry} from 'intel-fp';
 
-export const lte = curry(2, (b: number, a: number) => a <= b);
+export const lte = curry(2, (b:number, a:number) => a <= b);
 
-export const lt = curry(2, (b: number, a: number) => a < b);
+export const lt = curry(2, (b:number, a:number) => a < b);
 
-export const gte = curry(2, (b: number, a: number) => a >= b);
+export const gte = curry(2, (b:number, a:number) => a >= b);
 
-export const gt = curry(2, (b: number, a: number) => a > b);
+export const gt = curry(2, (b:number, a:number) => a > b);
 
-export const times = curry(2, (a: number, b: number) => a * b);
+export const times = curry(2, (a:number, b:number) => a * b);
 
-export const add = curry(2, (a: number, b: number) => a + b);
+export const add = curry(2, (a:number, b:number) => a + b);
 
-export const minus = curry(2, (a: number, b: number) => a - b);
+export const minus = curry(2, (a:number, b:number) => a - b);
 
-export const div = curry(2, (a: number, b: number) => a / b);
+export const div = curry(2, (a:number, b:number) => a / b);
