3fda6362fef493e568d101a73c9abc15388b3adb
Joe Grund <joe.grund@intel.com>
2017-03-16 16:58:32 -0400

    HYD-7247 Update math to not compile
    The math module will never be a root node when compiling.
    
    Because of this, do not transpile to ES5 during build time.
    
      - Remove transpilation code.
      - Switch to jest.
      - Remove fp dep.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- run_test.sh
+++ run_test.sh
@@ -3,7 +3,7 @@
 . ~/.nvm/nvm.sh
 
 NODE_VERSIONS="
-4
+6
 stable
 "
 
@@ -11,8 +11,6 @@ for node_version in $NODE_VERSIONS
 do
     nvm use $node_version
     rm -rf node_modules
-    npm i
-    npm run cover -- --reporter=cobertura
-    mv ./coverage/cobertura-coverage.xml ../coverage
-    mv *results*.xml ../results
+    yarn install
+    yarn run cover
 done
